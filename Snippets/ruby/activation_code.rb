4a4a8c75499282f59031e1fbaaa66b7f758773a3
lcampos <lcampos@strix.com.ve>
2018-04-29 23:06:36 -0430

    a simple snippet about a random password key generator

---
+++ Snippets/ruby/activation_code.rb
@@ -0,0 +1,5 @@
+# Generates a random string from a set of easily readable characters
+def generate_activation_code(size = 7)
+  charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
+  (0...size).map{ charset.to_a[rand(charset.size)] }.join
+end
