639515847adc3020f0eefb5646c57ea53678f5c8
lcampos <lcampos@strix.com.ve>
2018-06-27 15:53:45 -0400

    an array problem to solve, not quite there yet

---
+++ Exercises/Ruby/hourglass.rb
@@ -0,0 +1,52 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr)
+
+     max = 0
+     sum = 0
+    for i in 0..(arr.size()-2-1)
+        for y in 0..(arr.size()-2-1)
+
+            a = arr[i][y]
+            b = arr[i][y+1]
+            c = arr[i][y+2]
+            d = arr[i+1][y+1]
+            e = arr[i+2][y]
+            f = arr[i+2][y+1]
+            g = arr[i+2][y+2]
+
+            sum = a + b + c + d + e + f + g
+
+            if max <= sum
+                max = sum
+                puts i
+                puts y
+            end
+
+            sum = 0
+
+        end
+
+    end
+
+    return max
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+arr = Array.new(6)
+
+6.times do |i|
+    arr[i] = gets.rstrip.split(' ').map(&:to_i)
+end
+
+result = hourglassSum arr
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
