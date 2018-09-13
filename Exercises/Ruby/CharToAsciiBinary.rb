014021cf370fc9e07be70e986916e592bce61401
lcampos <lcampos@strix.com.ve>
2018-09-13 12:34:13 -0400

    a method to pass from character to 7 bits ascii

---
+++ Exercises/Ruby/CharToAsciiBinary.rb
@@ -0,0 +1,60 @@
+
+def translateAsciiBinary(letter)
+
+    if(letter == "A"){return "1000001"} 	 
+    if(letter == "B"){return "1000010"}
+    if(letter == "C"){return "1000011"}
+    if(letter == "D"){return "1000100"}
+    if(letter == "E"){return "1000101"}
+    if(letter == "F"){return "1000110"}
+    if(letter == "G"){return "1000111"}
+    if(letter == "H"){return "1001000"}
+    if(letter == "I"){return "1001001"}
+    if(letter == "J"){return "1001010"}
+    if(letter == "K"){return "1001011"}
+    if(letter == "L"){return "1001100"}
+    if(letter == "M"){return "1001101"}
+    if(letter == "N"){return "1001110"}
+    if(letter == "O"){return "1001111"}
+    if(letter == "P"){return "1010000"}
+    if(letter == "Q"){return "1010001"}
+    if(letter == "R"){return "1010010"}
+    if(letter == "S"){return "1010011"}
+    if(letter == "T"){return "1010100"}
+    if(letter == "U"){return "1010101"}
+    if(letter == "V"){return "1010110"}
+    if(letter == "W"){return "1010111"}
+    if(letter == "X"){return "1011000"}
+    if(letter == "Y"){return "1011001"}
+    if(letter == "Z"){return "1011010"}
+    if(letter == "a"){return "1100001"}
+    if(letter == "b"){return "1100010"}
+    if(letter == "c"){return "1100011"}
+    if(letter == "d"){return "1100100"}
+    if(letter == "e"){return "1100101"}
+    if(letter == "f"){return "1100110"}
+    if(letter == "g"){return "1100111"}
+    if(letter == "h"){return "1101000"}
+    if(letter == "i"){return "1101001"}
+    if(letter == "j"){return "1101010"}
+    if(letter == "k"){return "1101011"}
+    if(letter == "l"){return "1101100"}
+    if(letter == "m"){return "1101101"}
+    if(letter == "n"){return "1101110"}
+    if(letter == "o"){return "1101111"}
+    if(letter == "p"){return "1110000"}
+    if(letter == "q"){return "1110001"}
+    if(letter == "r"){return "1110010"}
+    if(letter == "s"){return "1110011"}
+    if(letter == "t"){return "1110100"}
+    if(letter == "u"){return "1110101"}
+    if(letter == "v"){return "1110110"}
+    if(letter == "w"){return "1110111"}
+    if(letter == "x"){return "1111000"}
+    if(letter == "y"){return "1111001"}
+    if(letter == "z"){return "1111010"}
+
+    STDERR.puts ("Letra invalida")
+    return "X"
+
+end
