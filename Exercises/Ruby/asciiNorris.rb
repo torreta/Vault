a6239fd22251335a766b95fed8fc23631c78f0d2
lcampos <lcampos@strix.com.ve>
2018-09-13 14:54:13 -0400

    unary chuckKeyboard exercise

---
+++ Exercises/Ruby/asciiNorris.rb
@@ -0,0 +1,143 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+@message = gets.chomp
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+STDERR.puts @message
+
+# define translation
+# http://roubaixinteractive.com/PlayGround/Binary_Conversion/The_Characters.asp
+def translateAsciiBinary(letter)
+
+    if(letter == "A")then return ("1000001"); end
+    if(letter == "B")then return ("1000010"); end
+    if(letter == "C")then return ("1000011"); end
+    if(letter == "D")then return ("1000100"); end
+    if(letter == "E")then return ("1000101"); end
+    if(letter == "F")then return ("1000110"); end
+    if(letter == "G")then return ("1000111"); end
+    if(letter == "H")then return ("1001000"); end
+    if(letter == "I")then return ("1001001"); end
+    if(letter == "J")then return ("1001010"); end
+    if(letter == "K")then return ("1001011"); end
+    if(letter == "L")then return ("1001100"); end
+    if(letter == "M")then return ("1001101"); end
+    if(letter == "N")then return ("1001110"); end
+    if(letter == "O")then return ("1001111"); end
+    if(letter == "P")then return ("1010000"); end
+    if(letter == "Q")then return ("1010001"); end
+    if(letter == "R")then return ("1010010"); end
+    if(letter == "S")then return ("1010011"); end
+    if(letter == "T")then return ("1010100"); end
+    if(letter == "U")then return ("1010101"); end
+    if(letter == "V")then return ("1010110"); end
+    if(letter == "W")then return ("1010111"); end
+    if(letter == "X")then return ("1011000"); end
+    if(letter == "Y")then return ("1011001"); end
+    if(letter == "Z")then return ("1011010"); end
+    if(letter == "a")then return ("1100001"); end
+    if(letter == "b")then return ("1100010"); end
+    if(letter == "c")then return ("1100011"); end
+    if(letter == "d")then return ("1100100"); end
+    if(letter == "e")then return ("1100101"); end
+    if(letter == "f")then return ("1100110"); end
+    if(letter == "g")then return ("1100111"); end
+    if(letter == "h")then return ("1101000"); end
+    if(letter == "i")then return ("1101001"); end
+    if(letter == "j")then return ("1101010"); end
+    if(letter == "k")then return ("1101011"); end
+    if(letter == "l")then return ("1101100"); end
+    if(letter == "m")then return ("1101101"); end
+    if(letter == "n")then return ("1101110"); end
+    if(letter == "o")then return ("1101111"); end
+    if(letter == "p")then return ("1110000"); end
+    if(letter == "q")then return ("1110001"); end
+    if(letter == "r")then return ("1110010"); end
+    if(letter == "s")then return ("1110011"); end
+    if(letter == "t")then return ("1110100"); end
+    if(letter == "u")then return ("1110101"); end
+    if(letter == "v")then return ("1110110"); end
+    if(letter == "w")then return ("1110111"); end
+    if(letter == "x")then return ("1111000"); end
+    if(letter == "y")then return ("1111001"); end
+    if(letter == "z")then return ("1111010"); end
+
+    if(letter == " ")then return ("0100000"); end
+
+    if(letter == "!")then return ("0100001"); end
+    if(letter == "\"")then return ("0100010"); end
+    if(letter == "#")then return ("0100011"); end
+    if(letter == "$")then return ("0100100"); end
+    if(letter == "%")then return ("0100101"); end
+    if(letter == "&")then return ("0100110"); end
+    if(letter == "'")then return ("0100111"); end
+    if(letter == "(")then return ("0101000"); end
+    if(letter == ")")then return ("0101001"); end
+    if(letter == "*")then return ("0101010"); end
+    if(letter == "+")then return ("0101011"); end
+    if(letter == ",")then return ("0101100"); end
+    if(letter == "-")then return ("0101101"); end
+    if(letter == ".")then return ("0101110"); end
+    if(letter == "/")then return ("0101111"); end
+    if(letter == "0")then return ("0110000"); end
+    if(letter == "1")then return ("0110001"); end
+    if(letter == "2")then return ("0110010"); end
+    if(letter == "3")then return ("0110011"); end
+    if(letter == "4")then return ("0110100"); end
+    if(letter == "5")then return ("0110101"); end
+    if(letter == "6")then return ("0110110"); end
+    if(letter == "7")then return ("0110111"); end
+    if(letter == "8")then return ("0111000"); end
+    if(letter == "9")then return ("0111001"); end
+    if(letter == ":")then return ("0111010"); end
+    if(letter == ";")then return ("0111011"); end
+    if(letter == "<")then return ("0111100"); end
+    if(letter == "=")then return ("0111101"); end
+    if(letter == ">")then return ("0111110"); end
+    if(letter == "?")then return ("0111111"); end
+    if(letter == "@")then return ("1000000"); end
+
+    STDERR.puts ("Letra invalida  #{letter}")
+    return "X"
+
+end
+
+# translate all caracters on input
+@binariMessage = ""
+
+@message_array = @message.split("")
+
+@message_array.each do |letter|
+# concat its binary as a long ass binary
+    @binariMessage = @binariMessage + translateAsciiBinary(letter)
+
+end
+
+# cicle to translate to unary as chuck said
+@unaryMessage = ""
+@lastLetter = ""
+@counter = 0
+
+@binariMessage = @binariMessage.split("")
+
+@binariMessage.each do |letra|
+
+  if letra != @lastLetter
+      @lastLetter = letra
+      if letra  == "1"
+        @unaryMessage = @unaryMessage + " 0 "
+      end
+      if letra  == "0"
+        @unaryMessage = @unaryMessage + " 00 "
+      end
+  end
+
+   @unaryMessage = @unaryMessage + "0"
+  STDERR.puts @unaryMessage
+end
+
+
+puts @unaryMessage.strip
