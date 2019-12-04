9160368eb5a67d88a78d0f52b4c3d7b270f088b6
Luis Campos <luiscampostorreta@gmail.com>
2019-12-04 00:39:40 -0400

    exercise with source

--- Exercises/React/timer.js
+++ Exercises/React/timer.js
@@ -1,4 +1,57 @@
 //Write a Timer component in react (function) that prints the current Time and updates it every second
 //Do it using react function components.
 
+// https://medium.com/better-programming/building-a-simple-countdown-timer-with-react-4ca32763dda7
+/*
+ * A simple React component
+ */
+class Application extends React.Component {
+    state = {
+        minutes: 3,
+        seconds: 0,
+    }
 
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            const { seconds, minutes } = this.state
+
+            if (seconds > 0) {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
+                }))
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(this.myInterval)
+                } else {
+                    this.setState(({ minutes }) => ({
+                        minutes: minutes - 1,
+                        seconds: 59
+                    }))
+                }
+            } 
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.myInterval)
+    }
+
+    render() {
+        const { minutes, seconds } = this.state
+        return (
+            <div>
+                { minutes === 0 && seconds === 0
+                    ? <h1>Busted!</h1>
+                    : <h1>Time Remaining: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+                }
+            </div>
+        )
+    }
+}
+
+
+/*
+ * Render the above component into the div#app
+ */
+React.render(<Application />, document.getElementById('app'));
