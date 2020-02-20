d33ad8b23c5ca65ceb620342933e75876690d242
Luis Campos <luiscampostorreta@gmail.com>
2020-02-19 23:56:47 -0400

    deleting checkbox

--- Interview/PagoPrueba/pagoprueba-front/src/SignIn.js
+++ Interview/PagoPrueba/pagoprueba-front/src/SignIn.js
@@ -4,8 +4,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -120,10 +118,6 @@ class SignIn extends React.Component {
             id="password"
             autoComplete="current-password"
             onChange={this.handleChange}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
           />
           <Button
             type="submit"
