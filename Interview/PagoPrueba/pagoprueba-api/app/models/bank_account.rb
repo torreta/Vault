c5cd00e8a20000d2a64ce853f4cfdfddfba977e2
Luis Campos <luiscampostorreta@gmail.com>
2020-02-20 02:14:51 -0400

    maped relation with bank account and transaction

--- Interview/PagoPrueba/pagoprueba-api/app/models/bank_account.rb
+++ Interview/PagoPrueba/pagoprueba-api/app/models/bank_account.rb
@@ -1,4 +1,6 @@
 class BankAccount < ApplicationRecord
   belongs_to :bank
   belongs_to :user
+  has_many :sent_transactions, :class_name => 'Transaction', :foreign_key => 'sender_account_id'
+  has_many :received_transactions, :class_name => 'Transaction', :foreign_key => 'receiver_acount_id'
 end
