7289174e987e480dd31a36efe43c4f4a42336551
Luis Campos <luiscampostorreta@gmail.com>
2020-02-19 22:22:01 -0400

    adding routes to project

---
+++ Interview/PagoPrueba/pagoprueba-front/src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+const Routes = () => {
+
+    
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={SignIn} />
+                    <Route path='/signup' component={SignUp} />
+                </Switch>
+            </BrowserRouter>
+        );
+    
+}
+
+export default Routes;
