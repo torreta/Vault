77bd952ac8ed9cb7687c9462837d57408308cb76
Luis Campos <luiscampostorreta@gmail.com>
2020-02-20 02:15:56 -0400

    last changes to schema made by transacctions

--- Interview/PagoPrueba/pagoprueba-api/db/schema.rb
+++ Interview/PagoPrueba/pagoprueba-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200220053944) do
+ActiveRecord::Schema.define(version: 20200220060314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,20 @@ ActiveRecord::Schema.define(version: 20200220053944) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["profiles_id"], name: "index_profiles_on_profiles_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.float "amount"
+    t.bigint "sender_account_id"
+    t.bigint "receiver_account_id"
+    t.bigint "sender_id"
+    t.bigint "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receiver_account_id"], name: "index_transactions_on_receiver_account_id"
+    t.index ["receiver_id"], name: "index_transactions_on_receiver_id"
+    t.index ["sender_account_id"], name: "index_transactions_on_sender_account_id"
+    t.index ["sender_id"], name: "index_transactions_on_sender_id"
   end
 
   create_table "users", force: :cascade do |t|
