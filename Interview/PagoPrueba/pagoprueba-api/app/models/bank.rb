02239642530f7450f44ad301cf13d05eef3f68ed
Luis Campos <luiscampostorreta@gmail.com>
2024-01-09 23:39:27 -0400

    pruning

--- Interview/PagoPrueba/pagoprueba-api/app/models/bank.rb
+++
@@ -1,4 +0,0 @@
-class Bank < ApplicationRecord
-    has_many :bank_accounts
-    has_many :users, through: :bank_accounts
-end
--- Interview/PagoPrueba/pagoprueba-api/app/models/transaction.rb
+++
@@ -1,6 +0,0 @@
-class Transaction < ApplicationRecord
-  belongs_to :sender_account, :class_name => 'BankAccount'
-  belongs_to :receiver_account, :class_name => 'BankAccount'
-  belongs_to :sender, :class_name => 'User'
-  belongs_to :receiver, :class_name => 'User'
-end
