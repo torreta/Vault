4bd2f75d1b26016aadd63a17f1b63ede1c7928f7
Luis Campos <luiscampostorreta@gmail.com>
2024-03-25 18:53:01 -0400

    shotttening

--- utilities/TaxesSeeder.php
+++ utilities/TaxesSeeder.php
@@ -14,25 +14,12 @@ class TaxesSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('taxes')->insert([
-            'id' => 1,
-            'name' => 'SIN IVA',
-            'percentage' => 0
-        ]);
-        DB::table('taxes')->insert([
-            'id' => 2,
-            'name' => 'IVA 16%',
-            'percentage' => 0.1600
-        ]);
-        DB::table('taxes')->insert([
-            'id' => 3,
-            'name' => 'IVA 8%',
-            'percentage' => 0.0800
-        ]);
-        DB::table('taxes')->insert([
-            'id' => 4,
-            'name' => 'IVA 15%',
-            'percentage' => 0.1500
-        ]);
+        $taxes = [
+            ['id' => 1, 'name' => 'SIN IVA', 'percentage' => 0],
+            ['id' => 2, 'name' => 'IVA 16%', 'percentage' => 0.1600],
+            ['id' => 3, 'name' => 'IVA 8%', 'percentage' => 0.0800],
+            ['id' => 4, 'name' => 'IVA 15%', 'percentage' => 0.1500],
+        ];
+        DB::table('taxes')->insert($taxes);
     }
 }
