739aa747acd421d0dd514adbeb9453f4ec38fdd9
Luis Campos <luiscampostorreta@gmail.com>
2024-04-08 17:57:14 -0400

    flash helper update

--- utilities/flash_helper.rb
+++ utilities/flash_helper.rb
@@ -12,6 +12,24 @@ module FlashHelper
 			flash.now[:success] = 'Registro de perfil exitoso ' + @current_user[:user][:email]
 		when 'successful_update'
 			flash.now[:success] = 'Datos actualizados correctamente'
+		when 'successful_login'
+			flash.now[:success] = 'Bienvenido de nuevo ' + @current_user[:user][:email]
+		when 'successful_register'
+			flash.now[:success] = 'Registro de perfil exitoso ' + @current_user[:user][:email]
+		when 'successful_medical_register'
+			flash.now[:success] = 'Registro de perfil médico exitoso ' + @current_user[:user][:email]
+			flash.now[:success] = 'Datos actualizados correctamente'
+		when 'Registro_Correcto'
+			flash.now[:success] = 'Se suscribio correctamente al plan '	
+			flash.now[:success] = 'Registro de perfil de servicios varios exitoso '
+		when 'Fallo_cus'
+			flash.now[:error] = 'Usted no posee cuenta cliente'
+		when 'Medical_error'
+			flash.now[:error] = 'Usted no posee cuenta de Medico'
+		when 'Generic_error'
+			flash.now[:error] = 'Usted no posee cuenta Servicios Varios '
+		when 'successful_update_medical_profile'
+			flash.now[:success] = 'Datos actualizados correctamente'
 		else
 
 			if action_do != nil
@@ -21,6 +39,5 @@ module FlashHelper
 		end
 
 	end
-
 
 end
