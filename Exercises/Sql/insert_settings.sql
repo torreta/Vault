9c79d2cd3e777649e38a5f6e32a1ac3859529951
Luis Campos <luiscampostorreta@gmail.com>
2024-09-09 23:17:57 -0400

    quick change for replace

--- Exercises/Sql/insert_settings.sql
+++ Exercises/Sql/insert_settings.sql
@@ -29,7 +29,7 @@ truncate gopharma.new_products ;
 INSERT INTO gopharma.new_products (bar_code, name, description, brand_id, presentation_id,  product_unit_id, catalogue_code, tax_id)SELECT upper(ipc.barcode), upper(ipc.name), upper(ipc.description), ipc.brandId, ipc.typesPresentationId, ipc.unitMeasurementId, ipc.code, if(dsp.tax_id = 2,1,2) as tax_id FROM pos_development.dbo_storage_products dsp right join gopharma_catalogue.inventory_products_catalogue ipc on dsp.code_bar = ipc.barcode where dsp.description is not null GROUP by dsp.code_bar;
 
 truncate product_images;
-insert into product_images (product_id, image)  select id as product_id, concat(bar_code,".png") as image from new_products;
+insert or replace into product_images (product_id, image)  select id as product_id, concat(bar_code,".png") as image from new_products;
 
 
 SET FOREIGN_KEY_CHECKS = 1;
