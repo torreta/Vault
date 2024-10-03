ece9925cddf96b37fc42ca3932ed20903fb9a721
Luis Campos <luiscampostorreta@gmail.com>
2024-10-03 18:11:33 -0400

    updating tables to better reflect what i want

--- Exercises/Sql/dresses_loans_database.sql
+++ Exercises/Sql/dresses_loans_database.sql
@@ -66,3 +66,60 @@ create table loan_statuses (
   updated_at timestamp with time zone default now(),
   comment text
 );
+
+create table statuses (
+  id bigint primary key generated always as identity,
+  name text not null
+);
+
+alter table loan_statuses
+add column status_id bigint references statuses (id),
+drop status;
+
+alter table loans
+add column history_id bigint references loan_statuses (id);
+
+alter table loan_statuses
+add column user_id bigint references users (id);
+
+create table client_notes (
+  id bigint primary key generated always as identity,
+  client_id bigint references clients (id),
+  user_id bigint references users (id),
+  note text not null,
+  created_at timestamp with time zone default now()
+);
+
+alter table loans
+add column amount numeric(10, 2) not null;
+
+create table product_costs (
+  id bigint primary key generated always as identity,
+  product_id bigint references products (id),
+  cost numeric(10, 2) not null,
+  description text,
+  created_at timestamp with time zone default now()
+);
+
+alter table products
+add column total_cost numeric(10, 2) default 0;
+
+create
+or replace function update_total_cost () returns trigger as $$
+BEGIN
+    UPDATE products
+    SET total_cost = (SELECT COALESCE(SUM(cost), 0) FROM product_costs WHERE product_id = NEW.product_id)
+    WHERE id = NEW.product_id;
+    RETURN NEW;
+END;
+$$ language plpgsql;
+
+create trigger update_product_total_cost
+after insert
+or delete
+or
+update on product_costs for each row
+execute function update_total_cost ();
+
+alter table product_costs
+add column user_id bigint references users (id);
